150cb2d1c86a7f02f7df1c373870b1b2ce27dc72
markoclimako <mkclimako@gmail.com>
2023-07-05 18:26:04 -0300

    req 3 implement layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './components/Layout';
 import Home from './pages/home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Home /> } />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 }
