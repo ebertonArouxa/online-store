820f8e8792eaffd2715733723e2de2669d23efd1
markoclimako <mkclimako@gmail.com>
2023-07-07 19:06:50 -0300

    req 10

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,59 +1,54 @@
 import { useEffect, useState } from 'react';
-import { CartProduct, ProductType } from '../../types';
+import CartProducts from '../../components/CartProducts';
+import { ProductType } from '../../types';
 
 function Cart() {
-  const [cartProducts, setCartProducts] = useState<{ [key: string]: CartProduct }>({});
-
+  const [cartProducts, setCartProducts] = useState<ProductType[]>([]);
   useEffect(() => {
     const productsFromStorage = localStorage.getItem('products');
     if (productsFromStorage) {
       const products = JSON.parse(productsFromStorage);
-      refatoreCartItems(products);
+      setCartProducts(products);
     }
   }, []);
 
-  const refatoreCartItems = (products: ProductType[]) => {
-    const countItems = products.reduce((acc, product) => {
-      const { title, price } = product;
-      acc[title] = acc[title] || { quantity: 0, price: 0 };
-      acc[title].quantity++;
-      acc[title].price += Number(price);
-      return acc;
-    }, {} as { [key: string]: CartProduct });
-    setCartProducts(countItems);
+  const handleIncreaseButton = (cartProduct:ProductType) => {
+    const productToUpdate = cartProducts.find((prod) => prod.id === cartProduct.id);
+    if (productToUpdate) {
+      const updateCartProducts = cartProducts
+        .map((productItem) => (productItem.id === productToUpdate.id
+          ? { ...productItem, quantity: productItem.quantity + 1 } : productItem));
+      setCartProducts(updateCartProducts);
+    }
   };
-  return (
-    Object.keys(cartProducts).length === 0
-      ? (
-        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
-      ) : (
-        Object.keys(cartProducts).map((itemName) => {
-          const { quantity, price } = cartProducts[itemName];
-          return (
-            <div
-              key={ itemName }
-            >
-              <h2
-                data-testid="shopping-cart-product-name"
-              >
-                { itemName }
 
-              </h2>
-              <h3>
-                Preço:
-                R$
-                {' '}
-                { price }
-              </h3>
-              <h3
-                data-testid="shopping-cart-product-quantity"
-              >
-                {quantity }
-              </h3>
-            </div>
-          );
-        })
-      )
+  const handleDecreaseButton = (cartProduct:ProductType) => {
+    const productToUpdate = cartProducts.find((prod) => prod.id === cartProduct.id);
+    if (productToUpdate && cartProduct.quantity > 1) {
+      const updateCartProducts = cartProducts
+        .map((productItem) => (productItem.id === productToUpdate.id
+          ? { ...productItem, quantity: productItem.quantity - 1 } : productItem));
+      setCartProducts(updateCartProducts);
+    }
+  };
+
+  const handleDeleteButton = (cartProduct:ProductType) => {
+    const deleteProduct = cartProducts.filter((prod) => prod.id !== cartProduct.id);
+    setCartProducts(deleteProduct);
+  };
+  if (cartProducts.length === 0) {
+    return (
+      <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+    );
+  }
+
+  return (
+    <CartProducts
+      cartProducts={ cartProducts }
+      handleDecreaseButton={ handleDecreaseButton }
+      handleDeleteButton={ handleDeleteButton }
+      handleIncreaseButton={ handleIncreaseButton }
+    />
 
   );
 }
